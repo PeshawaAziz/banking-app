fdd0a2f547ead44c3a8dc83856911ffed675d363
PeshawaAziz <modern3p2017@gmail.com>
2025-03-17 13:53:03 +0330

    feat: implement fields and methods of TransactionAccount class

--- bank/TransactionAccount.java
+++ bank/TransactionAccount.java
@@ -1,4 +1,26 @@
 package bank;
 
-public class TransactionAccount {
+public class TransactionAccount extends BankAccount {
+    private final double overDraftLimit = 500;
+
+    public TransactionAccount(String accountHolderName, String accountNumber, double initialBalance) {
+        super(accountNumber, accountHolderName, initialBalance);
+    }
+
+    @Override
+    public void calculateInterest() {
+        System.out.println("There is no interest for this account.");
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount >= 0)
+            if (balance + overDraftLimit >= amount)
+                balance -= amount;
+            else
+                System.out.println("Reached overdraft limit!");
+        else
+            System.out.println("The amount to be withdrawn cannot be negative!");
+    }
+
 }
