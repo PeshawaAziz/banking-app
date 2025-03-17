0617b66e69dc33cd9992c858244da3a32a911edc
PeshawaAziz <modern3p2017@gmail.com>
2025-03-17 14:04:08 +0330

    feat: implement the methods and fields of BankCustomers class

--- bank/BankCustomers.java
+++ bank/BankCustomers.java
@@ -1,4 +1,30 @@
 package bank;
 
+import java.util.ArrayList;
+
 public class BankCustomers {
+    private ArrayList<BankAccount> accountsList;
+
+    public void addAccount(BankAccount account) {
+        accountsList.add(account);
+    }
+
+    public void showAllBalances() {
+        for (BankAccount account : accountsList) {
+            System.out.println(account.getAccountHolderName() + " [" + account.getAccountNumber() + "] => Balance: "
+                    + account.balance + "$");
+        }
+    }
+
+    public BankAccount findAccount(String accountNumber) {
+        for (BankAccount account : accountsList) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                System.out.println(account.getAccountHolderName() + " [" + account.getAccountNumber() + "]");
+                return account;
+            }
+        }
+
+        System.out.println("No account was found.");
+        return null;
+    }
 }
